bea3780069df460364a80c96afa7d84d025e2f68
tonyrob <claudioroblest@gmail.com>
2014-04-01 19:57:58 -0430

    COMM1_PROGRAMA
    COMMITTED DE PROG. 1

---
+++ practicaphp.cpp
@@ -0,0 +1,23 @@
+                         //PRIMERA VERSION
+
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+int n1,n2, sum;
+
+int main()
+{
+cout<<"BIENVENIDO. MI NOMBRE ES AIBELIS ASTIDIAS"<<endl<<endl;
+{
+                   cout<<"Sumemos dos numeros enteros..."<<endl<<endl;
+cout<< "Ingrese el primer numero "<<endl;
+cin>> n1;
+cout<< "Ingrese el segundo numero "<<endl;
+cin>> n2; 
+sum=n1+n2;
+cout<< "La suma de los numeros ingresados es: "<< sum <<endl;
+cout<< "Hasta Luego..." <<endl;
+       getche ();
+     }       
+}
